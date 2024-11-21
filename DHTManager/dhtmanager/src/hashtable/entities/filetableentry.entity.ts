557e56fe9406733b42137879f036bd1b907d0557
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-11-21 20:31:11 -0300

    updated file table entry

--- DHTManager/dhtmanager/src/hashtable/entities/filetableentry.entity.ts
+++ DHTManager/dhtmanager/src/hashtable/entities/filetableentry.entity.ts
@@ -3,9 +3,7 @@ export class FileTableEntry {
     name: string
     path: string
     size: number
-    peer_id: number
-    description: string
-    tags: Array<string>
+    peerIp: number
     createdAt: Date
     updatedAt: Date
 }
