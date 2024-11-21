939f57c36aabb0ebdb57cd9e29bd44821ab46c01
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-11-21 20:30:58 -0300

    changed upload file controller

--- DHTManager/dhtmanager/src/resources/resources.controller.ts
+++ DHTManager/dhtmanager/src/resources/resources.controller.ts
@@ -2,18 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, Upl
 import { ResourcesService } from './resources.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Resource } from './entities/resource.entity';
+import { HashtableService } from 'src/hashtable/hashtable.service';
+
+// ### Dev notes ### //
+// -> this is using resolution 001 and 002
+//
 
 @Controller('resources')
 export class ResourcesController {
-  constructor(private readonly resourcesService: ResourcesService) {}
+  constructor(private readonly resourcesService: ResourcesService, private readonly hashtableService: HashtableService) {}
   
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@UploadedFile() file: Express.Multer.File) {
-    return {
-      message: "File uploaded",
-      filePath: file.path
-    }
+    return this.hashtableService.create({ name: file.originalname, path: file.path, size: file.size})
   }
 
   @Get()
