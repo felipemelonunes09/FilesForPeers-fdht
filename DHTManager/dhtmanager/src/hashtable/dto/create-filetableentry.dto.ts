030bfa03ef997ac65f2442b1d923752a6c5be168
felipemelonunes09 <felipe.melonunes09@gmail.com>
2024-11-22 00:44:24 -0300

    -

--- DHTManager/dhtmanager/src/hashtable/dto/create-filetableentry.dto.ts
+++ DHTManager/dhtmanager/src/hashtable/dto/create-filetableentry.dto.ts
@@ -2,14 +2,10 @@ import { IsNumber, isNumber, IsObject, IsString } from "class-validator";
 
 
 export class CreateFileEntryDto {
-
     @IsString()
     name: string
-
     @IsString()
     path: string
-
     @IsNumber()
     size: number
-
 }
